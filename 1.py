edc55dd61ebaec56506de82059cbbe63ba2798fa
Chi Zhang <zc20041014@gmail.com>
2025-07-26 03:29:47 +0800

    lr

--- 1.py
+++ 1.py
@@ -1,3 +1,22 @@
+# Resize all the png files from 1000*600 to 250*150
+
 import os
-print(os.listdir('C:\\Users\\zc200\\MyFiles\\2024-Summer\\mos_test\\sim_vb'))
-print(os.listdir('C:\\Users\\zc200\\MyFiles\\2024-Summer\\mos_test\\dns_real_upp'))
+from PIL import Image
+def resize_images_in_directory(directory, size=(250, 150)):
+    for filename in os.listdir(directory):
+        if filename.endswith('.png'):
+            file_path = os.path.join(directory, filename)
+            with Image.open(file_path) as img:
+                img = img.resize(size)
+                img.save(file_path)
+                print(f'Resized {filename} to {size}')
+if __name__ == "__main__":
+    directory = '.'  # Current directory
+    for dir in os.listdir(directory):
+        try:
+            print(dir)
+            if os.path.isdir(dir):
+                resize_images_in_directory(dir)
+        except Exception as e:
+            print(f"Error processing directory: {dir}, Error: {e}")
+            continue
